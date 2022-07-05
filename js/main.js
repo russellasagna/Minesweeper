f2dffb9b9a145a7338e8c3d0d65e9573cc8ff13d
russellasagna <laganzon.r2@yahoo.com>
2022-07-05 19:25:21 -0400

    Add JS console logging; Update JS handleClick() for obsolete name attribute

--- js/main.js
+++ js/main.js
@@ -27,24 +27,44 @@ init();
 function init() {
     playBtn.textContent = ":)";
     playBtn.id = "play";
-    board = [...document.querySelectorAll("#board > div")];
-    boardSize = 9;
     tileClears = 0; 
     tileMines = 0;
     timeStatus = 0;
     timeElapsed = 0;
     gameStatus = null;
+    boardSize = 9;
+    board = [...document.querySelectorAll("#board > div")];
+    // map mines below
     board.forEach(function(tile) {
         tile.className = "";
-        let chance = Math.floor(Math.random() * 10);
-        if (chance < 4) { // #/10 the tile is a mine
-            tile.setAttribute("name", -1); // first tile is safe at idx 0
+        let chance = Math.floor(Math.random() * 100);
+        if (chance < 15) { // #/10 the tile is a mine
+            tile.textContent = "-1";
             tileMines++;
         } else {
-            tile.setAttribute("name", 0);
+            tile.textContent = "0";
             tileClears++;
         }
     });
+    console.log("[RISK] Total clears: " + tileClears);
+    console.log("[RISK] Total mines: " + tileMines);
+    console.warn("Adjusting state for first click being safe...");
+    // after mapping mines, adjust state for first click being safe
+    tileClears++;
+    tileMines--;
+    console.log("[SAFE] Total clears: " + tileClears);
+    console.log("[SAFE] Total mines: " + tileMines);
+    console.log(board);
+    boardMap = [];
+    for (let i = 0; i < boardSize; i++) {
+        const arr = [];
+        for (let j = 0; j < boardSize; j++) {
+            arr.push(board[i * j - 1]);
+            // console.log(arr);
+        }
+        boardMap.push(arr);
+    }
+    console.log(boardMap);
     // insert mines into array using -1
     // insert safe tiles into array using 0
     
@@ -64,17 +84,19 @@ function handleClick(evt) {
 
     // update tile when clicked
     const idx = tileEls.indexOf(evt.target);
-    if (tileClears + tileMines === Math.pow(boardSize, 2)) {
+    if (tileClears + tileMines === Math.pow(boardSize, 2)) { // first tile is safe
+        board[idx].textContent = "";
         board[idx].className = "clear";
         tileClears--;
     } else {
-        switch (board[idx].getAttribute("name")) {
+        switch (board[idx].textContent) {
             case "0": 
+                board[idx].textContent = "";
                 board[idx].className = "clear";
                 tileClears--;
-                console.log(tileClears);
                 break;
             case "-1": 
+                board[idx].textContent = "";
                 board[idx].className = "mine";
                 playBtn.textContent = "):";
                 playBtn.id = "retry";
@@ -82,4 +104,5 @@ function handleClick(evt) {
                 break;
         }
     }
+    render();
 }
